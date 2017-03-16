4eed1aaf0ccdbbdc7b8297d4eb0c6b45389dcccb
lgfrbcsgo <lgfrbcsgo@gmail.com>
2017-03-16 16:23:39 +0100

    Removed Hello-World code snippets

--- api/controllers/website.js
+++ api/controllers/website.js
@@ -10,31 +10,37 @@ module.exports = {
 };
 
 function getMatchingSites(req, res) {
+    console.log(req.swagger.params);
     res.status(501);
     res.json('Not implemented!');
 }
 
 function getWebsiteWithID(req, res) {
+    console.log(req.swagger.params);
     res.status(501);
     res.json('Not implemented!');
 }
 
 function downloadContent(req, res) {
+    console.log(req.swagger.params);
     res.status(501);
     res.json('Not implemented!');
 }
 
 function addWebsites(req, res) {
+    console.log(req.swagger.params);
     res.status(501);
     res.json('Not implemented!');
 }
 
 function updateWebsite(req, res) {
+    console.log(req.swagger.params);
     res.status(501);
     res.json('Not implemented!');
 }
 
 function deleteWebsite(req, res) {
+    console.log(req.swagger.params);
     res.status(501);
     res.json('Not implemented!');
 }
