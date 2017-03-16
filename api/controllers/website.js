a8e0dc8c25008f897a137e9d94ea77047abc2968
lgfrbcsgo <lgfrbcsgo@gmail.com>
2017-03-16 15:46:03 +0100

    Added dummy request handlers

---
+++ api/controllers/website.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+    getMatchingSites: getMatchingSites,
+    addWebsite: addWebsite,
+    getWebsiteWithID: getWebsiteWithID,
+    updateWebsite: updateWebsite,
+    downloadContent: downloadContent
+
+};
+
+function getMatchingSites(req, res) {
+    res.status(501);
+    res.json('Not implemented!');
+}
+
+function addWebsite(req, res) {
+    res.status(501);
+    res.json('Not implemented!');
+}
+
+function getWebsiteWithID(req, res) {
+    res.status(501);
+    res.json('Not implemented!');
+}
+
+function updateWebsite(req, res) {
+    res.status(501);
+    res.json('Not implemented!');
+}
+
+function downloadContent(req, res) {
+    res.status(501);
+    res.json('Not implemented!');
+}
