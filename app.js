cfea5508673f4670ef21e595623c1245eed7a271
lgfrbcsgo <lgfrbcsgo@gmail.com>
2017-03-16 21:35:55 +0100

    /api/swagger/swagger.yaml will now get compiled to /config/swagger.json

--- app.js
+++ app.js
@@ -5,18 +5,21 @@ var app = require('express')();
 module.exports = app; // for testing
 
 var config = {
-  appRoot: __dirname // required config
+    appRoot: __dirname, // required config
+    swaggerFile: __dirname + '/config/swagger.json'
 };
 
-SwaggerExpress.create(config, function(err, swaggerExpress) {
-  if (err) { throw err; }
+SwaggerExpress.create(config, function (err, swaggerExpress) {
+    if (err) {
+        throw err;
+    }
 
-  // install middleware
-  swaggerExpress.register(app);
+    // install middleware
+    swaggerExpress.register(app);
 
-  var port = process.env.PORT || 10010;
-  app.listen(port);
+    var port = process.env.PORT || 10010;
+    app.listen(port);
 
-  console.log('Server running at http://127.0.0.1:' + port);
+    console.log('Server running at http://127.0.0.1:' + port);
 
 });
