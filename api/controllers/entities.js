b3653d78bac57f1bbd53a337c01f375c3ce6979c
Tim Henkelmann <ga68vah@mytum.de>
2017-03-21 17:24:40 +0100

    added entities

--- api/controllers/entities.js
+++ api/controllers/entities.js
@@ -1,39 +1,59 @@
 'use strict';
+const path = require('path');
+var context = require(path.join(__dirname, '../../index.js')).getContext();
+var entities = context.component('dsap').module('entities');
 
 module.exports = {
     getAllEntities: getAllEntities,
     getEntityByID: getEntityByID,
-    addEntities: addEntities,
+    addEntity: addEntity,
     updateEntity: updateEntity,
     deleteEntity: deleteEntity
 };
 
 function getAllEntities(req, res) {
-    console.log(req.swagger.params);
-    res.status(501);
-    res.json('Not implemented!');
+    entities.findAllEntities().then(function(list) {
+        res.status(200).json(list);
+    }).catch(function(error) {
+        res.status(500).send(error);
+    });
 }
 
 function getEntityByID(req, res) {
-    console.log(req.swagger.params);
-    res.status(501);
-    res.json('Not implemented!');
+    var id = req.swagger.params.id.value;
+    entities.findEntitiesById(id).then(function(entityInfo) {
+        res.status(200).json(entityInfo);
+    }).catch(function(error) {
+        res.status(500).send(error);
+    });
 }
 
-function addEntities(req, res) {
-    console.log(req.swagger.params);
-    res.status(501);
-    res.json('Not implemented!');
+//add one entity
+function addEntity(req, res) {
+    var entityObject = req.body;
+    entities.addEntity(entityObject).then(function(list) {
+        res.status(200).json(list);
+    }).catch(function(error) {
+        res.status(500).send(error);
+    });
 }
 
+//update one entity
 function updateEntity(req, res) {
-    console.log(req.swagger.params);
-    res.status(501);
-    res.json('Not implemented!');
+    var id = req.swagger.params.id.value;
+    var entityObject = req.body;
+    entities.updateEntity(id, entityObject).then(function(list) {
+        res.status(200).json(list);
+    }).catch(function(error) {
+        res.status(500).send(error);
+    });
 }
 
 function deleteEntity(req, res) {
-    console.log(req.swagger.params);
-    res.status(501);
-    res.json('Not implemented!');
+    var id = req.swagger.params.id.value;
+    entities.deleteEntity(id).then(function(list) {
+        res.status(200).json(list);
+    }).catch(function(error) {
+        res.status(500).send(error);
+    });
 }
