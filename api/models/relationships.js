0e6130f8916a99f440f627f0460b47f8550a9787
Simon Zachau <zachau@in.tum.de>
2017-03-29 15:21:26 +0200

    relationships table

--- api/models/relationships.js
+++ api/models/relationships.js
@@ -2,13 +2,26 @@ module.exports = function (context) {
     return context.sequelize.define('relationships', {
         id: {
             type: context.Sequelize.UUID,
+            defaultValue: context.Sequelize.UUIDV4,
             primaryKey: true
         },
-        relationship_occurrence:{
-            type:context.Sequelize.INTEGER
+        subject_id: {
+            type: context.Sequelize.UUID
         },
-        relationship_quality:{
-            type:context.Sequelize.FLOAT
+        object_id: {
+            type: context.Sequelize.UUID
+        },
+        type_id: {
+            type: context.Sequelize.UUID
+        },
+        occurrence_id: {
+            type: context.Sequelize.INTEGER
+        },
+        confidence: {
+            type: context.Sequelize.FLOAT
+        },
+        relation: {
+            type: context.Sequelize.TEXT
         }
     }, {
         freezeTableName: true // Model tableName will be the same as the model name
