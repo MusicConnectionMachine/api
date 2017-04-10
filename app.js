96b37eaf4036dece10bc380a272a64b13f34b1b6
Hendrik Leppelsack <hendrik@leppelsack.de>
2017-04-10 16:46:24 +0200

    all api requests now require an api key (#92)

--- app.js
+++ app.js
@@ -2,18 +2,26 @@
 
 var SwaggerExpress = require('swagger-express-mw');
 var app = require('express')();
+var config = require('./config.js.template');
 module.exports = app; // for testing
 
 
-var config = {
+var swaggerConfig = {
     appRoot: __dirname, // required config
-    swaggerFile: __dirname + '/api/swagger/swagger.json'
+    swaggerFile: __dirname + '/api/swagger/swagger.json',
+    swaggerSecurityHandlers: {'DefaultSecurity': (req, authOrSecDef, scopesOrApiKey, cb) => {
+        if (scopesOrApiKey === config.api.apiKey) {
+            cb();
+        } else {
+            cb(new Error('Wrong or Missing API key'));
+        }
+    }},
 };
 
 
 require('./database.js').connect(null,function (context) {
 
-    SwaggerExpress.create(config, function (err, swaggerExpress) {
+    SwaggerExpress.create(swaggerConfig, function (err, swaggerExpress) {
         context.sequelize.sync({force: true}).then(function () {
 
             if (err) {
