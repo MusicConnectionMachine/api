1c3d68a19c0abaebf35e7694ee5cf831bcf9a092
Hendrik Leppelsack <hendrik@leppelsack.de>
2017-04-23 19:32:04 +0200

    --wip-- [skip ci]

--- api/controllers/search.js
+++ api/controllers/search.js
@@ -6,7 +6,8 @@ var search = require('../dsap/search.js')(context);
 module.exports = {
     searchEntities: searchEntities,
     searchRelations: searchRelations,
-    searchString: searchString
+    searchString: searchString,
+    searchConnected: searchConnected
 };
 
 function searchEntities(req, res) {
@@ -28,4 +29,22 @@ function searchRelations(req, res) {
 function searchString(req, res) {
     res.status(501);
     res.json('Not implemented!');
+}
+
+function searchConnected(req, res) {
+    const subject = req.swagger.params.subject.value;
+    const object = req.swagger.params.object.value;
+    const relation = req.swagger.params.relation.value;
+
+    const query = req.swagger.params.q.value;
+
+    const limit = req.swagger.params.limit.value;
+    const offset = req.swagger.params.offset.value;
+
+
+    return search.searchConnected(
+        subject, object, relation,
+        query,
+        {limit: limit, offset: offset}
+    ).then(result => res.status(200).json(result));
 }
--- api/dsap/search.js
+++ api/dsap/search.js
@@ -2,52 +2,124 @@
 module.exports = function (context) {
     const models =  context.models;
 
+    function searchArtists(searchString, options, relation) {
+        return models.artists.findAll({
+            where: {
+                name: {
+                    $like: '%' + searchString + '%'
+                }
+            },
+            order: [['id', 'DESC']],
+            limit: options.limit,
+            offset: options.offset
+        });
+    }
+
+    function searchInstruments(searchString, options) {
+        return models.instruments.findAll({
+            where: {
+                name: {
+                    $like: '%' + searchString + '%'
+                }
+            },
+            order: [['id', 'DESC']],
+            limit: options.limit,
+            offset: options.offset
+        });
+    }
+
+    function searchReleases(searchString, options) {
+        models.releases.findAll({
+            where: {
+                title: {
+                    $like: '%' + searchString + '%'
+                }
+            },
+            order: [['id', 'DESC']],
+            limit: options.limit,
+            offset: options.offset
+        })
+    }
+
+    function searchWorks(searchString, options) {
+        models.releases.findAll({
+            where: {
+                title: {
+                    $like: '%' + searchString + '%'
+                }
+            },
+            order: [['id', 'DESC']],
+            limit: options.limit,
+            offset: options.offset
+        })
+    }
+
     return {
         searchEntities: function(searchString, limit, offset) {
-            const artistsSearch = models.artists.findAll({
-                where: {
-                    name: {
-                        $like: '%' + searchString + '%'
-                    }
-                },
-                order: [['id', 'DESC']],
-                limit: limit,
-                offset: offset
-            }).then((results) => {
-                return { artists: results }
-            });
+            const artistsSearch = searchArtists(searchString, {limit: limit, offset: offset})
+                .then((results) => {
+                    return { artists: results }
+                });
 
-            const instrumentsSearch = models.instruments.findAll({
-                where: {
-                    name: {
-                        $like: '%' + searchString + '%'
-                    }
-                },
-                order: [['id', 'DESC']],
-                limit: limit,
-                offset: offset
-            }).then((results) => {
-                return { instruments: results }
-            });
+            const instrumentsSearch = searchInstruments(searchString, {limit: limit, offset: offset})
+                .then((results) => {
+                    return { instruments: results }
+                });
 
-            const releasesSearch = models.releases.findAll({
-                where: {
-                    title: {
-                        $like: '%' + searchString + '%'
-                    }
-                },
-                order: [['id', 'DESC']],
-                limit: limit,
-                offset: offset
-            }).then((results) => {
-                return { releases: results }
-            });
+            const releasesSearch = searchReleases(searchString, {limit: limit, offset: offset})
+                .then((results) => {
+                    return { releases: results }
+                });
 
             return Promise.all([
                 artistsSearch, instrumentsSearch, releasesSearch
             ]).then(function(result) {
                 return result;
             });
+        },
+
+        searchConnected: function(subject, object, relation, searchString, options) {
+            const artistSearch = models.artists.findAll({
+                where: {
+                    name: {
+                        $like: '%' + searchString + '%'
+                    },
+                    '$entity.relationshipEntities.id$': relation,
+                },
+                include: [{
+                    model: models.entities,
+                    include: [{
+                        model: models.relationshipEntities,
+                        include: [{
+                            model: models.relationships
+                        }]
+                    }]
+                }],
+                order: [['id', 'DESC']],
+                limit: options.limit,
+                offset: options.offset
+            }).then((result) => {
+                return {artists: result};
+            });
+
+            const workSearch = Promise.resolve({relationships: []});
+
+            const relationSearch = Promise.resolve({relationships: []});
+                /*models.relationships.findAll({
+                where: {
+                    title: {
+                        $like: '%' + searchString + '%'
+                    }
+                },
+                order: [['id', 'DESC']],
+                limit: options.limit,
+                offset: options.offset
+            });*/
+
+            return Promise.all([artistSearch, workSearch, relationSearch])
+                .then((results) => {
+                    return Object.assign({}, ...results);
+                });
         }
     }
 };
