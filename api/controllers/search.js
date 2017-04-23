77b82971114e70ce5ba82d3c26398d487e424e31
Hendrik Leppelsack <hendrik@leppelsack.de>
2017-04-23 12:28:01 +0200

    Initial entity search api (#114)
    * initial entity search api
    
    * remove console.log
    
    * limit and offset result

--- api/controllers/search.js
+++ api/controllers/search.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var context = require('../../database.js').getContext();
+var search = require('../dsap/search.js')(context);
 
 module.exports = {
     searchEntities: searchEntities,
@@ -9,8 +10,14 @@ module.exports = {
 };
 
 function searchEntities(req, res) {
-    res.status(501);
-    res.json('Not implemented!');
+    const query = req.swagger.params.q.value;
+    const limit = req.swagger.params.limit.value;
+    const offset = req.swagger.params.offset.value;
+    search.searchEntities(query, limit, offset).then(function(list) {
+        res.status(200).json(list);
+    }).catch(function(error) {
+        res.status(500).send(error);
+    });
 }
 
 function searchRelations(req, res) {
