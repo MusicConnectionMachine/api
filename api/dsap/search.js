f98ad9fdd1d883dce1b0ea55a9b6272bf9f37a28
Hendrik Leppelsack <hendrik@leppelsack.de>
2017-04-24 17:33:13 +0200

    case-insensitive search (#130)

--- api/dsap/search.js
+++ api/dsap/search.js
@@ -2,46 +2,74 @@
 module.exports = function (context) {
     const models =  context.models;
 
+    function searchArtists(searchString, options, relation) {
+        return models.artists.findAll({
+            where: {
+                name: {
+                    $ilike: '%' + searchString + '%'
+                }
+            },
+            order: [['id', 'DESC']],
+            limit: options.limit,
+            offset: options.offset
+        });
+    }
+
+    function searchInstruments(searchString, options) {
+        return models.instruments.findAll({
+            where: {
+                name: {
+                    $ilike: '%' + searchString + '%'
+                }
+            },
+            order: [['id', 'DESC']],
+            limit: options.limit,
+            offset: options.offset
+        });
+    }
+
+    function searchReleases(searchString, options) {
+        models.releases.findAll({
+            where: {
+                title: {
+                    $ilike: '%' + searchString + '%'
+                }
+            },
+            order: [['id', 'DESC']],
+            limit: options.limit,
+            offset: options.offset
+        })
+    }
+
+    function searchWorks(searchString, options) {
+        models.releases.findAll({
+            where: {
+                title: {
+                    $ilike: '%' + searchString + '%'
+                }
+            },
+            order: [['id', 'DESC']],
+            limit: options.limit,
+            offset: options.offset
+        })
+    }
+
     return {
         searchEntities: function(searchString, limit, offset) {
-            const artistsSearch = models.artists.findAll({
-                where: {
-                    name: {
-                        $like: '%' + searchString + '%'
-                    }
-                },
-                order: [['id', 'DESC']],
-                limit: limit,
-                offset: offset
-            }).then((results) => {
-                return { artists: results }
-            });
+            const artistsSearch = searchArtists(searchString, {limit: limit, offset: offset})
+                .then((results) => {
+                    return { artists: results }
+                });
 
-            const instrumentsSearch = models.instruments.findAll({
-                where: {
-                    name: {
-                        $like: '%' + searchString + '%'
-                    }
-                },
-                order: [['id', 'DESC']],
-                limit: limit,
-                offset: offset
-            }).then((results) => {
-                return { instruments: results }
-            });
+            const instrumentsSearch = searchInstruments(searchString, {limit: limit, offset: offset})
+                .then((results) => {
+                    return { instruments: results }
+                });
 
-            const releasesSearch = models.releases.findAll({
-                where: {
-                    title: {
-                        $like: '%' + searchString + '%'
-                    }
-                },
-                order: [['id', 'DESC']],
-                limit: limit,
-                offset: offset
-            }).then((results) => {
-                return { releases: results }
-            });
+            const releasesSearch = searchReleases(searchString, {limit: limit, offset: offset})
+                .then((results) => {
+                    return { releases: results }
+                });
 
             return Promise.all([
                 artistsSearch, instrumentsSearch, releasesSearch
